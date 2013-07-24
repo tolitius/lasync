6ed820764ce5f2dfff179d902dc015163ccb5472
anatoly <tolitius@gmail.com>
2013-07-24 10:51:46 -0400

    signing release for clojars. promoting to 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lasync "0.1.0-SNAPSHOT"
+(defproject lasync "0.1.1-SNAPSHOT"
   :description "executor service that blocks on .submit after the task queue limit is reached"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
