574d8553281fdc6f41d2cf7eb00646e5c621ba37
anatoly <tolitius@gmail.com>
2017-07-05 17:25:18 -0400

    releasing 0.1.9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lasync "0.1.8"
+(defproject lasync "0.1.9"
   :description "executor service that blocks on .submit after the task queue limit is reached"
   :url "https://github.com/tolitius/lasync"
   :license {:name "Eclipse Public License"
